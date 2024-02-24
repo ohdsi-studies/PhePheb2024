9627e5971642bbde568ef7249ce5d782ddf193e3
Jamie Weaver <jamieweaver@gmail.com>
2024-02-23 22:53:15 -0500

    added missing alzh cohort to package

--- extras/PackageMaintenance.R
+++ extras/PackageMaintenance.R
@@ -5,7 +5,7 @@ baseUrl <- keyring::key_get("BASE_URL")
 ROhdsiWebApi::authorizeWebApi(baseUrl, "windows")
 
 targetCohortIds <- 15476
-outcomeCohortIds <- c(15478, 15480, 15481, 15482, 15483, 15486, 15487, 15488, 15526, 15490, 15007, 15523)
+outcomeCohortIds <- c(15478, 15480, 15481, 15482, 15483, 15486, 15487, 15488, 15489, 15526, 15490, 15007, 15523)
 cohortIds <- c(targetCohortIds, outcomeCohortIds)
 
 cohortDefinitionSet <- ROhdsiWebApi::exportCohortDefinitionSet(
